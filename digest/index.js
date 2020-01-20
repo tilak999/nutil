262b5f4ae7ac1671d5d7508e04fa55be09fefbec
Tilak Sasmal <tilaks@bitwiseglobal.net>
2020-01-20 13:51:59 +0530

    added proper path resolution

--- digest/index.js
+++ digest/index.js
@@ -1,5 +1,5 @@
 module.exports = function(args){
     const crypto = require('crypto')
-    let hash = crypto.createHash(args[1]).update('some_string').digest("hex")
+    let hash = crypto.createHash(args[1]).update(args[2]).digest("hex")
     console.log(hash)
 }
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
+
 const fs = require('fs')
+const path = require('path')
 
 const input = process.argv
-const modulePath = './'+input[2]
-const moduleName = input[2]
 
-if(fs.existsSync(modulePath)){ 
-    input.splice(0,2);
-    if(typeof require(modulePath) == 'function') require(modulePath)(input)
-    else {
-        console.log("'"+moduleName+"'",'Module must export a function.')
-        console.log("See Example: https://gist.github.com/")
+if (input[2]) {
+
+    const modulePath = path.join(process.mainModule.path, input[2])
+    const moduleName = input[2]
+
+    console.log('working', modulePath);
+    if (fs.existsSync(modulePath)) {
+        input.splice(0, 2);
+        if (typeof require(modulePath) == 'function') require(modulePath)(input)
+        else {
+            console.log("'" + moduleName + "'", 'Module must export a function.')
+            console.log("See Example: https://gist.github.com/")
+        }
     }
+} else {
+    console.log("Usage:")
+    console.log("npx","nutil","[Module]","[Module Args]")
 }
